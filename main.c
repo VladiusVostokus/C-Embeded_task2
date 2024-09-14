d16ede9b2699d77b7ff6cb2c1f962a5bb87ddd2b
VladiusVostokus <kovalenkov222@gmail.com>
2024-09-14 12:16:30 +0300

    Enter the value of a from input

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-   printf("Hello, World!\n");
-   return 0;
+    int a;
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    printf("You have enter %d\n", a);
+    return 0;
 }
