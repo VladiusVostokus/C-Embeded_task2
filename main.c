2c7e998488bf02a2dbd768ad0ef15722994f7a51
VladiusVostokus <kovalenkov222@gmail.com>
2024-09-14 12:47:34 +0300

    calculate cos(a) + cos(2a) + cos(6a) + cos(7a)

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main() {
-    int a;
+    double a;
     printf("Enter the value of a: ");
-    scanf("%d",&a);
-    printf("You have enter %d\n", a);
+    scanf("%lf",&a);
+    double expression1 = cos(a) + cos(a * 2) + cos(a * 6) + cos(a * 7);
+    printf("cos(a) + cos(2a) + cos(6a) + cos(7a)= %lf\n", expression1);
     return 0;
 }
